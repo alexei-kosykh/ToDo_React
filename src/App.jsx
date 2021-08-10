078809f1129e0218bd8cf8354b995305a5e68fad
Alexei Kosykh <aleksei-kosykh@mail.ru>
2021-08-10 13:41:15 +0300

    Realized pattern for left sidebar. Used some props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import List from "./Components/List/List";
+import {
+  listSvg,
+  addSvg,
+  checkSvg,
+  closeSvg,
+  editSvg,
+  removeSvg,
+} from "./assets/PackSvg";
+
 function App() {
   return (
     <div className="block-note">
       <div className="block-note__sidebar">
-        <button>Hello</button>
+        <List items={[{ icon: listSvg, title: "Все задачи", active: true }]} />
+        <List
+          items={[
+            { color: "green", title: "Покупки" },
+            { color: "blue", title: "FrontEnd" },
+            { color: "pink", title: "Фильмы и сериалы" },
+          ]}
+        />
       </div>
-      <div className="block-note__notes"></div>
+
+      <div className="block-note__tasks"></div>
     </div>
   );
 }
