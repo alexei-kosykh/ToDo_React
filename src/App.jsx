c3e2979c1225ab9e0da5596b58b34416ab6e6882
Alexei Kosykh <aleksei-kosykh@mail.ru>
2021-08-10 09:35:59 +0300

    add some styles and wrapper

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="block-note">
+      <div className="block-note__sidebar">
+        <button>Hello</button>
+      </div>
+      <div className="block-note__notes"></div>
+    </div>
+  );
+}
+
+export default App;
