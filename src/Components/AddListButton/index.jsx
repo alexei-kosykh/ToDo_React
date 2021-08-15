1fdea9554390e149c0951010ae4b06cd139e959e
Alexei Kosykh <aleksei-kosykh@mail.ru>
2021-08-15 20:00:19 +0300

    Add modal value in lists

--- src/Components/AddListButton/index.jsx
+++ src/Components/AddListButton/index.jsx
@@ -6,9 +6,27 @@ import closeSvg from "../../assets/icon/closeSvg.svg";
 
 import "./AddListButton.scss";
 
-const AddListButton = ({ icon, title, colors }) => {
+const AddListButton = ({ icon, title, colors, onAdd }) => {
   const [showAddModal, setShowAddModal] = useState(false);
   const [selectedColor, setSelectedColor] = useState(colors[0].id);
+  const [inputValue, setInputValue] = useState("");
+
+  const addList = () => {
+    if (!inputValue) {
+      alert("Ввведите название списка");
+      return;
+    }
+    const color = colors.filter((c) => c.id === selectedColor)[0].name;
+    onAdd({
+      id: Math.random(),
+      title: inputValue,
+      color,
+    });
+    setShowAddModal(false);
+    setInputValue("");
+    setSelectedColor(colors[0].id);
+  };
+
   return (
     <>
       <List
@@ -29,11 +47,15 @@ const AddListButton = ({ icon, title, colors }) => {
             alt="Close"
             className="add-apear-block__close-btn"
           />
+
           <input
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
             className="area-input"
             type="text"
             placeholder="Название списка.."
           />
+
           <div className="add-apear-block__colors">
             {colors.map((color) => (
               <ColorCircle
@@ -44,7 +66,9 @@ const AddListButton = ({ icon, title, colors }) => {
               />
             ))}
           </div>
-          <button className="button">Добавить</button>
+          <button onClick={addList} className="button">
+            Добавить
+          </button>
         </div>
       )}
     </>
