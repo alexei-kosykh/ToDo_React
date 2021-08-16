0598a60dbd796ea3bd0d7f68584eaf11b3755b70
Alexei Kosykh <aleksei-kosykh@mail.ru>
2021-08-16 12:42:56 +0300

    Created tasks and styles. Added animation

--- src/assets/PackSvg.jsx
+++ src/assets/PackSvg.jsx
@@ -37,21 +37,3 @@ export const addSvg = (
     />
   </svg>
 );
-
-export const checkSvg = (
-  <svg
-    width="11"
-    height="8"
-    viewBox="0 0 11 8"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001"
-      stroke="white"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </svg>
-);
