a74d9c9b75c5f071a8ba175bb73609e72d15481a
Alexei Kosykh <aleksei-kosykh@mail.ru>
2021-08-16 12:42:56 +0300

    Created tasks and styles. Added animation

--- src/Components/Tasks/index.jsx
+++ src/Components/Tasks/index.jsx
@@ -1,5 +1,6 @@
 import "./Tasks.scss";
 import editSvg from "../../assets/icon/editSvg.svg";
+
 const Tasks = () => {
   return (
     <div className="block-note__tasks">
@@ -7,7 +8,31 @@ const Tasks = () => {
         Frontend
         <img src={editSvg} alt="Edit icon" />
       </h2>
-      <div className="block-note__tasks_items"></div>
+      <div className="block-note__tasks_items">
+        <div className="block-note__tasks_item-row">
+          <div className="block-note__tasks_checkbox">
+            <input id="check" type="checkbox" />
+            <label htmlFor="check">
+              <svg
+                width="11"
+                height="8"
+                viewBox="0 0 11 8"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001"
+                  stroke="#000"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </label>
+          </div>
+          <input value="ReactJS Hooks (useState, useEffect and ect.)" />
+        </div>
+      </div>
     </div>
   );
 };
